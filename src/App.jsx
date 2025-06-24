263afda223a4840d4a5ab6314c9203929e241f06
jrlehd <kantjgd@gmail.com>
2025-06-24 18:00:11 +0900

    bg 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,65 +11,64 @@ import Section4 from "./components/Section4";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import About from "./pages/About"; // ⬅️ 새로 만든 페이지 import
+import About from "./pages/About";
 import Brands from "./pages/Brands";
 import Product_List from "./pages/Product_List";
 import ProductDetail from "./pages/Product_Detail";
 import Vegicel from "./pages/Vegicel";
 import Jayeonha from "./pages/Jayeonha";
 import RNDPage from "./pages/RND";
-import Consult from "./pages/Consult"; 
-
-
+import Consult from "./pages/Consult";
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <ScrollToTop />
-      <Routes>
-        {/* 메인 페이지: fullpage.js로 구성된 섹션 */}
-        <Route
-          path="/"
-          element={
-            <ReactFullpage
-              scrollingSpeed={1000}
-              navigation
-              recordHistory={false}
-              credits={{ enabled: false }}
-              render={() => (
-                <ReactFullpage.Wrapper>
-                  <div className="section">
-                    <Section1 />
-                  </div>
-                  <div className="section">
-                    <Section2 />
-                  </div>
-                  <div className="section">
-                    <Section3 />
-                  </div>
-                  <div className="section">
-                    <Section4 />
-                  </div>
-                  <div className="section !h-fit !min-h-[250px] !py-0">
-                    <Footer />
-                  </div>
-                </ReactFullpage.Wrapper>
-              )}
-            />
-          }
-        />
-
-        {/* /about 정적 페이지 연결 */}
-        <Route path="/about" element={<About />} />
-        <Route path="/brands" element={<Brands />} />
-        <Route path="/products" element={<Product_List />} />
-        <Route path="/products/:slug" element={<ProductDetail />} />
-        <Route path="/rnd" element={<RNDPage />} />
-        <Route path="/brands/vegicel" element={<Vegicel />} />
-        <Route path="/brands/jayeonha" element={<Jayeonha />} />
-        <Route path="/consult" element={<Consult />} />
-      </Routes>
-    </Router>
+    <div className="bg-[#F5FFFB]"> {/* 전체 배경색 설정 */}
+      <Router>
+        <Header />
+        <ScrollToTop />
+        <Routes>
+          {/* 메인 페이지: fullpage.js로 구성된 섹션 */}
+          <Route
+            path="/"
+            element={
+              <ReactFullpage
+                scrollingSpeed={1000}
+                navigation
+                recordHistory={false}
+                credits={{ enabled: false }}
+                render={() => (
+                  <ReactFullpage.Wrapper>
+                    <div className="section">
+                      <Section1 />
+                    </div>
+                    <div className="section">
+                      <Section2 />
+                    </div>
+                    <div className="section">
+                      <Section3 />
+                    </div>
+                    <div className="section">
+                      <Section4 />
+                    </div>
+                    <div className="section !h-fit !min-h-[250px] !py-0">
+                      <Footer />
+                    </div>
+                  </ReactFullpage.Wrapper>
+                )}
+              />
+            }
+          />
+          {/* 기타 서브 페이지 */}
+          <Route path="/about" element={<About />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/products" element={<Product_List />} />
+          <Route path="/products/:slug" element={<ProductDetail />} />
+          <Route path="/rnd" element={<RNDPage />} />
+          <Route path="/brands/vegicel" element={<Vegicel />} />
+          <Route path="/brands/jayeonha" element={<Jayeonha />} />
+          <Route path="/consult" element={<Consult />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
