6fe56e9ee255e441e21a004b6a3ba007bebe5489
jrlehd <kantjgd@gmail.com>
2025-06-25 10:04:28 +0900

    Change about page description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,13 +39,13 @@ export default function About() {
             className="w-full max-w-[500px] aspect-[4/3] object-cover rounded-xl shadow"
           />
           <div>
-            <h2 className="text-3xl font-bold mb-4">자연과 사람을 연결하는 브랜드, 큐라엘</h2>
+            <h2 className="text-3xl font-bold mb-4">자연과 건강을 잇는 큐라엘</h2>
             <p className="text-gray-700 leading-relaxed">
               (주)큐라엘은 천연물 기반 항암 건강기능식품과 맞춤형 통합케어 솔루션을 제공하는 헬스케어 기업입니다.
-              ‘베지셀’, ‘커큐진’, ‘베리베라진’ 등 자연에서 얻은 건강 소재를 과학적으로 설계하고,
+              자연에서 얻은 건강 소재를 정밀하게 설계하고,
               1:1 상담과 식이·심리·생활 전반의 케어 서비스를 통해 암 환자의 삶의 질을 높이는 데 집중합니다.
               건강식품부터 상담, 출판, 미디어까지 연결된 토털 서비스로
-              보다 현실적이고 따뜻한 암 케어의 기준을 만들어가고 있습니다.
+              보다 체계적이고 과학적인 접근을 통해 암 케어의 기준을 만들어가고 있습니다.
             </p>
           </div>
         </div>
@@ -53,7 +53,7 @@ export default function About() {
         {/* 자연 */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mb-24">
           <div>
-            <h2 className="text-3xl font-bold mb-4">자연에서 출발해, 삶에 스며드는 건강</h2>
+            <h2 className="text-3xl font-bold mb-4">재료 그대로의 힘</h2>
             <p className="text-gray-700 leading-relaxed">
               큐라엘은 재료 그대로의 힘을 믿습니다.
               몸이 민감한 사람일수록 무엇을 먹는지가 중요하기에, 우리는 불필요한 첨가물이나 과한 가공을 배제하고 재료 본연의 성질을 최대한 살리는 방식을 선택합니다.
@@ -76,7 +76,7 @@ export default function About() {
             className="w-full max-w-[500px] aspect-[4/3] object-cover rounded-xl shadow"
           />
           <div>
-            <h2 className="text-3xl font-bold mb-4">천연물과 파이토케미컬 기반의 혁신,<br /> 암 치료의 새로운 기준을 세웁니다</h2>
+            <h2 className="text-3xl font-bold mb-4">천연물과 파이토케미컬 기반의 혁신</h2>
             <p className="text-gray-700 leading-relaxed">
               큐라엘은 자연에서 유래한 식물성 유효 성분을 과학적 근거에 기반해 정교하게 조합함으로써,
               암의 예방부터 치료, 회복에 이르기까지 전 과정을 통합 지원하는 글로벌 항암 헬스케어 기업을 지향합니다.
