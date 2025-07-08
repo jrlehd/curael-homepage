e50b6faf7b235de6601c00339b9e0f278ae45bbe
jrlehd <kantjgd@gmail.com>
2025-07-08 10:02:06 +0900

    더보기 url 수정

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -10,7 +10,7 @@ const products = {
     image: "/images/vegicel_section2.jpg",
     description:
       "베지셀은 신선한 야채와 과일 그대로를 담은 천연 식이 파이토케미컬 주스입니다. 항암 중에도 부담 없이 섭취할 수 있도록 무가당, 무첨가, 저열처리 방식으로 만들었으며, 세포 회복과 염증 완화, 면역 균형을 고려해 약사가 직접 설계한 처방형 블렌딩입니다.",
-    link: "/products/12025061911331",
+    link: "/products/vegicel",
   },
   자연하: {
     title: "자연을 닮은 건강한 흐름을 만듭니다",
@@ -18,7 +18,7 @@ const products = {
     image: "/images/jayeonha_section2.jpg",
     description:
       "자연의 원리와 흐름을 기반으로 몸과 삶의 회복을 돕는 천연물 기반 건강식품 브랜드입니다. 우리는 단순한 ‘건강식품’을 넘어, 회복·예방·면역·해독 중심의 자연주의 치유 식문화를 만들어갑니다.",
-    link: "/products/22025061911334",
+    link: "/products/jayeonha",
   },
   서적: {
     title: "환자와 함께 만든 치유의 기록",
@@ -26,7 +26,7 @@ const products = {
     image: "/images/book_section2.png",
     description:
       "환자가 스스로 암을 이해하고 치유의 주체가 되기를 바랐습니다. 병원 진료만으로는 채워지지 않는 정보와 방향을 책을 통해 전하고자 했습니다. 암을 두려워하기보다, 준비하고 실천할 수 있도록 돕고 싶었습니다. 그래서 그는 책을 ‘교육’이자 ‘회복의 길잡이’로 선택했습니다.",
-    link: "/products/32025061911331",
+    link: "/products/books",
   },
 };
 
